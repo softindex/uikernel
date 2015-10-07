dc12ae558ee83f8a9127eb880c8694ef06cd0497
sahara18 <ahantulin@adkernel.com>
2015-10-07 18:43:45 +0300

    Improved grid express api routes restriction

--- src/grid/models/gridExpressApi.js
+++ src/grid/models/gridExpressApi.js
@@ -15,84 +15,91 @@ var ValidationErrors = require('../../common/validation/ValidationErrors');
 /**
  * Form Express API for Grid model interaction
  *
- * @param {Object}    router
- * @param {string[]}  availableMethods
+ * @param {Object}              router
+ * @param {string[]|Function}   availableMethods
  * @return {GridExpressApi}
  * @constructor
  */
 function GridExpressApi(router, availableMethods) {
   if (!(this instanceof GridExpressApi)) {
-    return new GridExpressApi(router);
+    return new GridExpressApi(router, availableMethods);
+  }
+  if (availableMethods && Array.isArray(availableMethods)) {
+    availableMethods = function () {
+      return availableMethods;
+    };
   }
 
   var builderContext = this;
 
-  if (!availableMethods || availableMethods.indexOf('read') >= 0) {
-    router.get('/', function (req, res, next) {
-      var settings = {};
-      if (req.query.limit) {
-        settings.limit = req.query.limit;
-      }
-      if (req.query.offset) {
-        settings.offset = req.query.offset;
-      }
-      if (req.query.sort) {
-        settings.sort = JSON.parse(req.query.sort);
-      }
-      if (req.query.fields) {
-        settings.fields = JSON.parse(req.query.fields);
-      }
-      if (req.query.extra) {
-        settings.extra = JSON.parse(req.query.extra);
-      }
-      if (req.query.filters) {
-        settings.filters = JSON.parse(req.query.filters);
-      }
-      builderContext._getModel(req, res).read(settings, function (err, response) {
-        builderContext._result(err, response, req, res, next);
-      });
+  router.get('/', function (req, res, next) {
+    if (availableMethods && availableMethods(req).indexOf('read') === -1) {
+      return next();
+    }
+    var settings = {};
+    if (req.query.limit) {
+      settings.limit = req.query.limit;
+    }
+    if (req.query.offset) {
+      settings.offset = req.query.offset;
+    }
+    if (req.query.sort) {
+      settings.sort = JSON.parse(req.query.sort);
+    }
+    if (req.query.fields) {
+      settings.fields = JSON.parse(req.query.fields);
+    }
+    if (req.query.extra) {
+      settings.extra = JSON.parse(req.query.extra);
+    }
+    if (req.query.filters) {
+      settings.filters = JSON.parse(req.query.filters);
+    }
+    builderContext._getModel(req, res).read(settings, function (err, response) {
+      builderContext._result(err, response, req, res, next);
     });
-  }
+  });
 
-  if (!availableMethods || availableMethods.indexOf('isValidRecord') >= 0) {
-    router.get('/validation', function (req, res, next) {
-      var record = JSON.parse(req.query.record);
-      builderContext._getModel(req, res).isValidRecord(record, function (err, errors) {
-        builderContext._result(err, errors, req, res, next);
-      });
+  router.get('/validation', function (req, res, next) {
+    var record = JSON.parse(req.query.record);
+    builderContext._getModel(req, res).isValidRecord(record, function (err, errors) {
+      builderContext._result(err, errors, req, res, next);
     });
-  }
+  });
 
-  if (!availableMethods || availableMethods.indexOf('getRecord') >= 0) {
-    router.get('/:recordId', function (req, res, next) {
-      var cols = req.query.cols ? JSON.parse(req.query.cols) : null;
-      builderContext._getModel(req, res).getRecord(req.params.recordId, cols, function (err, response) {
-        builderContext._result(err, response, req, res, next);
-      });
+  router.get('/:recordId', function (req, res, next) {
+    if (availableMethods && availableMethods(req).indexOf('getRecord') === -1) {
+      return next();
+    }
+    var cols = req.query.cols ? JSON.parse(req.query.cols) : null;
+    builderContext._getModel(req, res).getRecord(req.params.recordId, cols, function (err, response) {
+      builderContext._result(err, response, req, res, next);
     });
-  }
+  });
 
-  if (!availableMethods || availableMethods.indexOf('update') >= 0) {
-    router.put('/', function (req, res, next) {
-      builderContext._getModel(req, res).update(req.body, function (err, response) {
-        if (err && !Array.isArray(err)) {
-          return builderContext._result(err, null, req, res, next);
-        }
-        builderContext._result(null, {data: response, error: err}, req, res, next);
-      });
+  router.put('/', function (req, res, next) {
+    if (availableMethods && availableMethods(req).indexOf('update') === -1) {
+      return next();
+    }
+    builderContext._getModel(req, res).update(req.body, function (err, response) {
+      if (err && !Array.isArray(err)) {
+        return builderContext._result(err, null, req, res, next);
+      }
+      builderContext._result(null, {data: response, error: err}, req, res, next);
     });
-  }
+  });
 
-  if (!availableMethods || availableMethods.indexOf('create') >= 0) {
-    router.post('/', function (req, res, next) {
-      builderContext._getModel(req, res).create(req.body, function (err, data) {
-        if (err && !(err instanceof ValidationErrors)) {
-          return builderContext._result(err, null, req, res, next);
-        }
-        builderContext._result(null, {data: data, error: err}, req, res, next);
-      });
+  router.post('/', function (req, res, next) {
+    if (availableMethods && availableMethods(req).indexOf('create') === -1) {
+      return next();
+    }
+    builderContext._getModel(req, res).create(req.body, function (err, data) {
+      if (err && !(err instanceof ValidationErrors)) {
+        return builderContext._result(err, null, req, res, next);
+      }
+      builderContext._result(null, {data: data, error: err}, req, res, next);
     });
-  }
+  });
 
   router.use(function (err, req, res, next) {
     builderContext._result(err, null, req, res, next);
