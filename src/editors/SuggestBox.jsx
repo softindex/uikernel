c8d02f3a8f2c574ea08b0d8381b6c87279ae87ee
sahara18 <axsq13@gmail.com>
2015-11-27 11:55:01 +0200

    Suggestbox usability improve, refactor

--- src/editors/SuggestBox.jsx
+++ src/editors/SuggestBox.jsx
@@ -11,317 +11,416 @@
 'use strict';
 
 var React = require('react');
+var Portal = require('../common/Portal');
 var utils = require('../common/utils');
 
-var OPTIONS_ELEMENT_ID = '__suggestBoxPopUp';
+var popupId = '__suggestBoxPopUp';
+var classes = {
+  option: '__suggestBoxPopUp-option',
+  optionFocused: '__suggestBoxPopUp-option-focused',
+  optionSelectable: '__suggestBoxPopUp-option-selectable',
+  optionGroup: '__suggestBoxPopUp-option-group',
+  optionHeader: '__suggestBoxPopUp-option-header',
+  optionSubitem: '__suggestBoxPopUp-option-subitem',
+  searchBlock: '__suggestBox-search'
+};
 
 var SuggestBoxEditor = React.createClass({
   propTypes: {
-    select: React.PropTypes.bool,
-    debounce: React.PropTypes.number,
+    disabled: React.PropTypes.bool,
     model: React.PropTypes.shape({
-      read: React.PropTypes.func
+      read: React.PropTypes.func,
+      getLabel: React.PropTypes.func
     }),
     onChange: React.PropTypes.func.isRequired,
     onLabelChange: React.PropTypes.func,
     value: React.PropTypes.any
   },
+
   getDefaultProps: function () {
     return {
-      select: false,
-      debounce: 0
+      disabled: false
     };
   },
+
   getInitialState: function () {
     return {
+      isOpened: false,
       options: [],
-      neatSearchTimeout: null
+      selectedOptionKey: null,
+      lastValidLabel: '',
+      value: null
     };
   },
+
   componentDidMount: function () {
-    this.setLabel(this.props);
-
-    $('body').on('mousedown', this.handleMouseDown);
-    // jQuery events not support "useCapture"
-    document.addEventListener('scroll', this.handleMouseScroll, true);
-  },
-  componentWillUnmount: function () {
-    $('body').off('mousedown', this.handleMouseDown);
-    // jQuery events not support "useCapture"
-    document.removeEventListener('scroll', this.handleMouseScroll, true);
-
-    if (this.state.options.length) {
-      $('#' + OPTIONS_ELEMENT_ID).remove();
-    }
-  },
-  componentWillReceiveProps: function (props) {
-    if (this.props.value !== props.value) {
-      this.setLabel(props);
-    }
-  },
-  componentDidUpdate: function (prevProps, prevState) {
-    var $popUpElement = $('#' + OPTIONS_ELEMENT_ID);
-    var $popUpTable = $popUpElement.find('.__suggestBoxPopUpTable');
-    var $containerElement;
-    var containerOffset;
-    var popupHeight;
-
-    if (!utils.isEqual(prevState.options, this.state.options)) {
-      if (this.state.options.length) {
-        $containerElement = $(this.refs.container.getDOMNode());
-        containerOffset = $containerElement.offset();
-
-        if (!$popUpElement.length) {
-          $popUpElement = $('<div id="' + OPTIONS_ELEMENT_ID + '"></div>');
-          $popUpTable = $('<div class="__suggestBoxPopUpTable"></div>');
-          $popUpElement.html($popUpTable);
-          $('body').append($popUpElement);
-        }
-
-        $popUpTable.html(this.getOptionsListHTML());
-        $popUpElement.css({
-          top: containerOffset.top + $containerElement.height(),
-          left: containerOffset.left,
-          minWidth: $containerElement.width()
-        });
-
-        // If window height is too short, expand list upwards
-        popupHeight = $popUpElement.height();
-        if (($popUpElement.offset().top + popupHeight) > $(window).height()) {
-          $popUpElement.css({
-            top: containerOffset.top - popupHeight
-          });
-        }
-      } else if ($popUpElement) {
-        $popUpElement.remove();
-      }
-    }
-  },
-  handleMouseDown: function (e) {
-    if (!this.isMounted() || e.button !== 0) {
-      return;
-    }
-
-    var input = this.refs.input.getDOMNode();
-    var $container = $(this.refs.container.getDOMNode());
-    var $popup = $('#' + OPTIONS_ELEMENT_ID);
-    var $target = $(e.target);
-
-    if (input !== document.activeElement && !this.state.options.length) {
-      return;
-    }
-
-    if ($popup.find($target).length) {
-      if (!$target.attr('data-key')) {
-        $target = $target.parent();
-      }
-      if ($target.hasClass('__suggestBoxPopUp-option-selectable')) {
-        var option = this.state.options[$target.attr('data-key')];
-        this.refs.input.getDOMNode().value = this._getLabel(option);
-        this.saveValue(option);
-      }
-    } else if ($target[0] !== $popup[0] && !$container.find($target).length) {
-      this.saveValueWithValidation();
-    }
-  },
-  handleMouseScroll: function (e) {
-    if (this.state.options.length && e.target.id !== OPTIONS_ELEMENT_ID) {
-      this.saveValueWithValidation();
-    }
-  },
-  handleChange: function (e) {
-    this.neatSearch(e.target.value, function (data) {
-      this.setState({options: data});
-    });
+    this._getLabelFromModel(this.props.value);
   },
 
-  toggleList: function () {
-    if (!this.props.select || this.props.disabled) {
-      return;
-    }
+  shouldComponentUpdate: function (nextProps, nextState) {
+    return !utils.isEqual(this.state.value, nextProps.value)
+      || this.state.selectedOptionKey !== nextState.selectedOptionKey
+      || this.state.isOpened !== nextState.isOpened
+      || this.state.options.length !== nextState.options.length;
+  },
 
-    if (this.state.options.length) {
-      this.setState({options: []}, function () {
-        this.refs.input.getDOMNode().focus();
-      });
-    } else {
-      this._openList();
+  componentWillReceiveProps: function (nextProps) {
+    if (!utils.isEqual(this.state.value, nextProps.value)) {
+      this.state.value = nextProps.value;
+      this._getLabelFromModel(nextProps.value);
     }
   },
 
-  _getLabel: function (option) {
+  _getOptionLabel: function (option) {
     return Array.isArray(option.label) ? option.label[option.label.length - 1] : option.label;
   },
 
-  _openList: function () {
-    this.search('', function (data) {
-      this.setState({options: data}, function () {
-        this.refs.input.getDOMNode().focus();
-      });
-    });
-  },
-
-  getOptionsListHTML: function () {
-    return this.state.options.reduce(function (result, option, key) {
-      var className = '__suggestBoxPopUp-option';
-      if (option.id) {
-        className += ' __suggestBoxPopUp-option-selectable';
-      }
-
-      switch (option.type) {
-        case 'group':
-        case 'header':
-        case 'subitem':
-          className += ' __suggestBoxPopUp-option-' + option.type;
-      }
-
-      return result + '<div data-key="' + key + '" class="' + className + '">' + (
-        Array.isArray(option.label) ? option.label.map(function (label) {
-          return '<div>' + label + '</div>';
-        }).join('') : '<div>' + utils.escape(option.label) + '</div>'
-      ) + '</div>';
-    }, '');
-  },
-
-  /**
-   * Save changes and shut down helps
-   *
-   * @param {Array} nextValue New SuggestBox value
-   */
-  saveValue: function (nextValue) {
-    this.setState({options: []});
-
-    if (!utils.isEqual(this.props.value, nextValue.id)) {
-      this.props.onChange(nextValue.id);
-      if (this.props.onLabelChange) {
-        this.props.onLabelChange(nextValue.label);
-      }
-    }
-    if (this.props.onBlur) {
-      this.props.onBlur();
+  _setLabelTo: function (label, markAsValid) {
+    this.refs.input.getDOMNode().value = label;
+    if (markAsValid) {
+      this.state.lastValidLabel = label;
     }
   },
 
-  /**
-   * Input text check and right save call
-   */
-  saveValueWithValidation: function () {
-    var input = this.refs.input.getDOMNode();
-    var value = input.value;
+  _getLabelFromModel: function (id) {
+    if (!id) {
+      return this._setLabelTo('');
+    }
 
-    if (!value) {
-      this.saveValue({
-        id: null,
-        value: ''
+    this.props.model.getLabel(id, function (err, label) {
+      if (err) {
+        throw err;
+      }
+      this._setLabelTo(label, true);
+    }.bind(this));
+  },
+
+  _updateList: function (searchPattern, cb) {
+    this._loadData(searchPattern, function (err, options) {
+      if (err) {
+        throw err;
+      }
+      this.setState({
+        options: options,
+        selectedOptionKey: null
+      }, function () {
+        this._scrollListTo('top');
+        if (typeof cb === 'function') {
+          cb();
+        }
       });
+    }.bind(this));
+  },
+
+  _loadData: utils.throttle(function (searchPattern, cb) {
+    this.props.model.read(searchPattern, cb);
+  }),
+
+  _openList: function (searchPattern, cb) {
+    if (this.props.disabled || this.state.isOpened) {
       return;
     }
+    this.setState({isOpened: true}, function () {
+      this.refs.input.getDOMNode().focus();
+      this.refs.input.getDOMNode().select();
 
-    this.search(value, function (data) {
-      // Do nothing if value has changed during the check
-      if (value !== input.value) {
-        return;
-      }
+      var $input = $(this.refs.input.getDOMNode());
+      var $popup = $('#' + popupId);
 
-      var resultIndex = utils.findIndex(data, function (option) {
-        return option.id && value === this._getLabel(option);
-      }.bind(this));
+      var inputOffset = $input.offset();
+      var inputWidth = $input.css('width');
+      var inputHeight = $input.css('height');
 
-      if (resultIndex < 0) {
-        this.saveValue({
-          id: null,
-          value: value
+      var offsetTop = inputOffset.top + parseInt(inputHeight);
+      var offsetLeft = inputOffset.left;
+
+      $popup
+        .css('minWidth', inputWidth)
+        .offset({
+          top: offsetTop,
+          left: offsetLeft
         });
-        return;
-      }
 
-      input.value = this._getLabel(data[resultIndex]);
-      this.saveValue(data[resultIndex]);
+      this._updateList(searchPattern || '', function () {
+        var selectedOptionKey = utils.findIndex(this.state.options, function (option) {
+          return utils.isEqual(option.id, this.props.value);
+        }.bind(this));
+        if (selectedOptionKey) {
+          this._focusOptionAndScrollIntoView(Number(selectedOptionKey));
+        }
+        if (typeof cb === 'function') {
+          cb();
+        }
+      }.bind(this));
     });
   },
 
-  search: function (label, cb) {
-    this.props.model.read(label, function (err, data) {
-      if (!this.isMounted()) {
-        return;
-      }
-      if (err) {
-        throw err;
-      }
-      cb.call(this, data);
-    }.bind(this));
+  _openListAndSelectAll: function () {
+    this._openList();
   },
 
-  neatSearch: function (label, cb) {
-    if (this.state.neatSearchTimeout) {
-      clearTimeout(this.state.neatSearchTimeout);
+  _closeList: function (shouldBlur) {
+    if (shouldBlur) {
+      this.refs.input.getDOMNode().blur();
     }
-    this.state.neatSearchTimeout = setTimeout(function () {
-      this.state.neatSearchTimeout = null;
-      this.search(label, cb);
-    }.bind(this), this.props.debounce);
+    if (!this.state.isOpened) {
+      return;
+    }
+    this.setState({
+      options: [],
+      selectedOptionKey: null,
+      isOpened: false
+    });
   },
 
-  setLabel: function (props) {
-    var value = props.value;
+  _toggleList: function () {
+    if (this.state.isOpened) {
+      this._closeList();
+    } else {
+      this._openList();
+    }
+  },
 
-    if (!value) {
-      this.refs.input.getDOMNode().value = '';
+  _selectOption: function (option) {
+    this.state.value = option.id;
+    this.props.onChange(option.id);
+    if (typeof this.props.onLabelChange === 'function') {
+      this.props.onLabelChange(option.label);
+    }
+    this._setLabelTo(this._getOptionLabel(option), true);
+    this.refs.input.getDOMNode().select();
+  },
+
+  _focusOption: function (key, shouldSelectOption) {
+    if (shouldSelectOption === true) {
+      this._selectOption(this.state.options[key]);
+    }
+    if (this.state.isOpened) {
+      this._focusOptionAndScrollIntoView(key);
+    } else {
+      this._openList(null, function () {
+        this._focusOptionAndScrollIntoView(key);
+      });
+    }
+  },
+
+  _focusOptionAndScrollIntoView: function (key) {
+    this.state.selectedOptionKey = key;
+    $('.' + classes.optionFocused).removeClass(classes.optionFocused);
+    $('.' + classes.option + '[data-key="' + key + '"]').addClass(classes.optionFocused);
+    var domOption = $('#' + popupId + ' li[data-key="' + key + '"]').get(0);
+    this._scrollListTo(domOption);
+  },
+
+  _focusNextOption: function () {
+    if (this.state.selectedOptionKey === null) {
+      this.state.selectedOptionKey = 0;
+      return this._focusOption(this.state.selectedOptionKey);
+    }
+
+    var key;
+    for (key = this.state.selectedOptionKey + 1; key < this.state.options.length; key++) {
+      if (this.state.options[key].id) {
+        return this._focusOption(key, true);
+      }
+    }
+    for (key = 0; key < this.state.selectedOptionKey + 1; key++) {
+      if (this.state.options[key].id) {
+        return this._focusOption(key, true);
+      }
+    }
+  },
+
+  _focusPrevOption: function () {
+    if (this.state.selectedOptionKey === null) {
+      this.state.selectedOptionKey = 0;
+      return this._focusOption(this.state.selectedOptionKey);
+    }
+
+    var key;
+    for (key = this.state.selectedOptionKey - 1; key >= 0; key--) {
+      if (this.state.options[key].id) {
+        return this._focusOption(key, true);
+      }
+    }
+    for (key = this.state.options.length - 1; key > this.state.selectedOptionKey - 1; key--) {
+      if (this.state.options[key].id) {
+        return this._focusOption(key, true);
+      }
+    }
+  },
+
+  _scrollListTo: function (target) {
+    var container = $('#' + popupId).get(0);
+
+    if (target === 'top') {
+      container.scrollTop = 0;
       return;
     }
 
-    props.model.getLabel(value, function (err, label) {
-      var curValue = props.value;
+    if (target.offsetTop - container.scrollTop >= container.clientHeight - target.clientHeight) {
+      container.scrollTop = target.offsetTop - container.clientHeight + target.clientHeight;
+    } else if (target.offsetTop - container.scrollTop < 0) {
+      container.scrollTop = target.offsetTop;
+    }
+  },
 
-      if (!this.isMounted()) {
-        return;
+  _onDocumentMouseDown: function (e, isOwner) {
+    if (e.button !== 0) {
+      return;
+    }
+    var $target = $(e.target);
+    if (isOwner) {
+      if (!$target.hasClass(classes.option)) {
+        $target = $target.parent();
       }
-      if (err) {
-        throw err;
+      if ($target.hasClass(classes.optionSelectable) && this.state.isOpened) {
+        this._selectOption(this.state.options[$target.attr('data-key')]);
+        this._closeList(true);
       }
+    } else if (!$target.parents('.' + classes.searchBlock).length) {
+      this._setLabelTo(this.state.lastValidLabel);
+      this._closeList(true);
+    }
+  },
 
-      if (curValue === value) {
-        this.refs.input.getDOMNode().value = label;
+  _onDocumentMouseScroll: function (e, isOwner) {
+    if (!isOwner) {
+      if (this.state.isOpened) {
+        this._setLabelTo(this.state.lastValidLabel);
       }
-    }.bind(this));
+      this._closeList(true);
+    }
+  },
+
+  _onInputKeyDown: function (e) {
+    if (this.props.disabled) {
+      return;
+    }
+
+    switch (e.keyCode) {
+      case 40: // down arrow
+        e.preventDefault();
+        if (!this.state.isOpened) {
+          return this._openList();
+        }
+        this._focusNextOption();
+        break;
+      case 38: // up arrow
+        e.preventDefault();
+        if (!this.state.isOpened) {
+          return this._openList();
+        }
+        this._focusPrevOption();
+        break;
+      case 9: // eslint-disable-line no-fallthrough
+        if (!this.state.isOpened) {
+          break;
+        }
+      case 13: // enter
+        e.preventDefault();
+        if (this.state.selectedOptionKey === null) {
+          this._setLabelTo(this.state.lastValidLabel);
+        } else {
+          this._selectOption(this.state.options[this.state.selectedOptionKey]);
+        }
+        this._closeList();
+        break;
+      case 27: // escape
+        e.preventDefault();
+        this._setLabelTo(this.state.lastValidLabel);
+        this._closeList();
+        break;
+    }
+  },
+
+  _onInputValueChange: function (e) {
+    if (this.props.disabled) {
+      return;
+    }
+
+    if (this.state.isOpened) {
+      this._updateList(e.target.value);
+    } else {
+      this._openList(e.target.value);
+    }
   },
 
   focus: function () {
-    if (this.props.select) {
-      this._openList();
-    } else {
-      this.refs.input.getDOMNode().focus();
-    }
+    this.refs.input.getDOMNode().focus();
   },
 
   render: function () {
-    var wrapperClasses = ['suggest-box'];
-    var arrowClasses = ['arrow'];
+    var optionsPopup = null;
 
-    if (this.props.select) {
-      wrapperClasses.push('select');
+    if (this.state.isOpened) {
+      var options = this.state.options.map(function (option, key) {
+        var optionClassName = classes.option;
+        if (key === this.state.selectedOptionKey) {
+          optionClassName += ' ' + classes.optionFocused;
+        }
+
+        if (option.id) {
+          optionClassName += ' ' + classes.optionSelectable;
+        }
+
+        switch (option.type) {
+          case 'group':
+            optionClassName += ' ' + classes.optionGroup;
+            break;
+          case 'header':
+            optionClassName += ' ' + classes.optionHeader;
+            break;
+          case 'subitem':
+            optionClassName += ' ' + classes.optionSubitem;
+            break;
+        }
+
+        return (
+          <li
+            key={key}
+            data-key={key}
+            onMouseOver={this._focusOption.bind(null, key)}
+            className={optionClassName}
+          >
+            {
+              Array.isArray(option.label) ? option.label.map(function (label, columnKey) {
+                return <div key={columnKey}>{label}</div>;
+              }) : <div>{option.label}</div>
+            }
+          </li>
+        );
+      }.bind(this));
+
+      optionsPopup = (
+        <Portal
+          id={popupId}
+          onDocumentMouseDown={this._onDocumentMouseDown}
+          onDocumentMouseScroll={this._onDocumentMouseScroll}
+          className='__suggestBoxPopUp'
+        >
+          <ul>{options}</ul>
+        </Portal>
+      );
     }
-    if (this.state.options.length) {
-      arrowClasses.push('up');
-    }
+
     return (
-      <div
-        className={wrapperClasses.join(' ')}
-        ref="container"
-      >
-        <div className="search" onClick={this.toggleList}>
+      <div className='__suggestBox'>
+        <div className={classes.searchBlock}>
           <input
-            {...utils.omit(this.props, ['value', 'onBlur'])}
-            ref="input"
-            type="text"
-            onChange={this.handleChange}
+            {...utils.omit(this.props, ['model', 'value', 'onChange', 'onLabelChange'])}
+            ref='input'
+            type='text'
+            onClick={this._openList}
+            onFocus={this._openListAndSelectAll}
+            onKeyDown={this._onInputKeyDown}
+            onChange={this._onInputValueChange}
           />
-          {this.props.select ?
-            <div className="select-btn">
-              <div className={arrowClasses.join(' ')}></div>
-            </div>
-          : null}
+          <div onClick={this._toggleList} className="select-btn">
+            <div className={'arrow' + (this.state.isOpened ? ' up' : '')}></div>
+          </div>
         </div>
+        {optionsPopup}
       </div>
     );
   }
