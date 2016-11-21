f76768ddd69d245397b806e3a18cd2c544e543c6
a.bondar <dgoodua@gmail.com>
2016-11-21 15:56:38 +0200

    settings limit, offset to int

--- src/grid/models/gridExpressApi.js
+++ src/grid/models/gridExpressApi.js
@@ -30,10 +30,10 @@ function GridExpressApi() {
     read: [function (req, res, next) {
       var settings = {};
       if (req.query.limit) {
-        settings.limit = req.query.limit;
+        settings.limit = parseInt(req.query.limit);
       }
       if (req.query.offset) {
-        settings.offset = req.query.offset;
+        settings.offset = parseInt(req.query.offset);
       }
       if (req.query.sort) {
         settings.sort = JSON.parse(req.query.sort);
