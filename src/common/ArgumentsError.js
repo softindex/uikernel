7614dc3ef58014478dfb711c3acf175a0125324a
Dmitry Blyus <blyus@uikernel.io>
2017-01-20 12:15:07 +0200

    Fixed ArgumentsError in FireFox

--- src/common/ArgumentsError.js
+++ src/common/ArgumentsError.js
@@ -7,9 +7,17 @@
  */
 
 function ArgumentsError(message) {
+  Error.call(this, message);
+
+  this.name = 'ArgumentsError';
   this.message = message;
   this.status = this.statusCode = 422;
-  Error.captureStackTrace(this, ArgumentsError);
+
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(this, ArgumentsError);
+  } else {
+    this.stack = (new Error()).stack;
+  }
 }
 
 ArgumentsError.prototype = Object.create(Error.prototype);
