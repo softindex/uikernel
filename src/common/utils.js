5e2fb8888fd8cc35997062cddeca387f92bb0356
MaxGenash <123maks189@mail.ru>
2017-05-17 16:23:30 +0300

    fixed problem with IE in utils.getstack

--- src/common/utils.js
+++ src/common/utils.js
@@ -428,10 +428,19 @@ exports.getRecordChanges = function (model, data, changes, newChanges) {
 };
 
 exports.getStack = function () {
-  return new Error().stack
-    .split('\n')
-    .slice(2) // Error message, getStack
-    .join('\n');
+  //we add here try..catch because in IE Error.stack is available only
+  // for thrown errors: https://msdn.microsoft.com/ru-ru/library/windows/apps/hh699850.aspx
+  try {
+    throw new Error();
+  } catch (e) {
+    if (e.stack) {        //Error.stack is unavailable in old browsers
+      return e.stack
+        .split('\n')
+        .slice(2)         //here we delete rows 'Error' and 'at getStack(utils.js:427)'
+        .join('\n');
+    }
+    return '';
+  }
 };
 
 exports.warn = function (message) {
