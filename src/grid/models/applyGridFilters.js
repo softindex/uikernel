65a38fea479aef849de01ac7bb74f2f6ad1023b8
Kate Veryha <kveryha@adkernel.com>
2018-09-07 16:59:46 +0300

    * apply grid filters by merging with previously applied ones * now returns instance of model'c class

--- src/grid/models/applyGridFilters.js
+++ src/grid/models/applyGridFilters.js
@@ -6,25 +6,25 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import callbackify from '../../common/callbackify';
-import utils from '../../common/utils';
-
 /**
  * Defines filter values while reading Grid model data
  *
- * @param {AbstractGridModel} model       Grid model
- * @param {Object}            filters     Filter values
+ * @param {AbstractGridModel} model                   Grid model
+ * @param {Object}            filters                 Filter values
  */
 function applyGridFilters(model, filters) {
-  if (model instanceof utils.Decorator) {
-    model = Object.getPrototypeOf(model);
-  }
-  return utils.decorate(model, {
-    read: callbackify(async options => {
-      options.filters = filters;
-      return await model.read(options);
-    })
-  });
+  return Object.assign(
+    Object.create(model),
+    {
+      read: async function (options) {
+        options.filters = {
+          ...filters,
+          ...options.filters,
+        };
+        return await model.read(options);
+      }
+    }
+  );
 }
 
 export default applyGridFilters;
