62e44de41f24fbb3660faaabfe479e824bab86d6
Max Genash <max.genash@gmail.com>
2019-01-16 13:16:06 +0200

    Improved NumberEditor

--- src/editors/Number.js
+++ src/editors/Number.js
@@ -7,22 +7,28 @@
  */
 
 import floatValidator from '../common/validation/rules/float';
+import integerValidator from '../common/validation/rules/number';
 import utils from '../common/utils';
 import {findDOMNode} from 'react-dom';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const isInvalidFloat = floatValidator(null, null, true);
-
 class NumberEditor extends React.Component {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
+    onBlur: PropTypes.func,
     value: PropTypes.oneOfType([
       PropTypes.number,
       // String should be allowed, because when we start typing negative number,
       // there is appearing a warning in console after '-' symbol
       PropTypes.string
-    ])
+    ]),
+    // if true - inputted value will be parsed as float, else it will be parsed as integer removing fractional part
+    enableFractionalPart: PropTypes.bool
+  };
+
+  static defaultProps = {
+    enableFractionalPart: true
   };
 
   constructor(props) {
@@ -30,6 +36,9 @@ class NumberEditor extends React.Component {
     this.state = {
       value: props.value
     };
+    // Edge doesn't support "target.valueAsNumber"
+    this._numberParser = this.props.enableFractionalPart ? Number.parseFloat : Number.parseInt;
+    this._isInvalidNumber = (this.props.enableFractionalPart ? floatValidator : integerValidator)(null, null, true);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -38,28 +47,39 @@ class NumberEditor extends React.Component {
     }
   }
 
-  _onChangeHandler(e) {
+  _onBlurHandler = (e) => {
+    const input = findDOMNode(this.input);
+    if (!utils.isEqual(this.state.value, input.value)) {
+      input.value = this.state.value;
+    }
+    if (this.props.onBlur) {
+      this.props.onBlur(e);
+    }
+  };
+
+  _onChangeHandler = (e) => {
     const target = e.target;
-    const valueAsNumber = parseFloat(target.value); // Edge doesn't support "target.valueAsNumber"
+    const valueAsNumber = this._numberParser(target.value) ;
     if (target.value === '' && target.validity.valid) { // Invalid number set empty string and valid=false to event
       this.state.value = null;
-    } else if (isInvalidFloat(valueAsNumber)) {
+    } else if (this._isInvalidNumber(valueAsNumber)) {
       this.state.value = '';
     } else {
       this.state.value = valueAsNumber;
     }
 
     this.props.onChange(this.state.value);
-  }
+  };
 
   render() {
     return (
       <input
         step="any"
-        {...utils.omit(this.props, 'value')}
+        {...utils.omit(this.props, ['value', 'enableFractionalPart'])}
         type="number"
         ref={(input) => this.input = input}
-        onChange={::this._onChangeHandler}
+        onChange={this._onChangeHandler}
+        onBlur={this._onBlurHandler}
         defaultValue={this.props.value}
       />
     );
