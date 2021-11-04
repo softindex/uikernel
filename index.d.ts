cf573211ecd4fa2d8ec4b91d353f14b28d502f39
kir4ik <vlados.linkoln@gmail.com>
2021-11-04 17:03:09 +0200

    added validator types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,36 @@
 declare module 'uikernel' {
-  const x: any;
-  export = x;
+  export interface TValidator<TField, TRecord> {
+    field: (field: TField, cb: (value: any) => string | undefined) => TValidator<TField, TRecord>;
+    asyncField: (
+      field: TField,
+      cb: (value: any) => Promise<string | undefined>
+    ) => TValidator<TField, TRecord>;
+    fields: (
+      fields: TField[],
+      cb: (record: Partial<TRecord>, errors: any) => any
+    ) => TValidator<TField, TRecord>;
+    asyncFields: (
+      fields: TField[],
+      cb: (record: Partial<TRecord>, errors: any) => Promise<any>
+    ) => TValidator<TField, TRecord>;
+    asyncDependence: (fields: TField[]) => TValidator<TField, TRecord>;
+    getValidationDependency: (fields: TField[]) => TField[];
+    isValidRecord: (record: Partial<TRecord>) => Promise<any>;
+  }
+
+  export interface TValidatorConstructor {
+    new <TField, TRecord>(): TValidator<TField, TRecord>;
+    create<TField, TRecord>(): TValidator<TField, TRecord>;
+  }
+
+  interface TUIKernel {
+    Validator: TValidatorConstructor;
+    createValidator: <TField, TRecord>() => TValidator<TField, TRecord>;
+    [key: string]: any;
+  }
+
+  const x: TUIKernel;
+  export default x;
 }
 
 declare module 'uikernel/lib/common/utils' {
