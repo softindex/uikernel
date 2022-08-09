8c3c956c3e531993d35c3c9ef73522c8f6056063
kir4ik <vlados.linkoln@gmail.com>
2022-08-09 22:33:22 +0300

    added `FormModel` type & fix method declarations & more strict type

--- index.d.ts
+++ index.d.ts
@@ -5,40 +5,67 @@ declare module 'uikernel' {
   }
 
   export class ValidationErrors<TField extends string> {
-    static createFromJSON: (
-      jsonObject: Record<TField, (ValidationJSONError | string)[]>
-    ) => ValidationErrors<TField>;
-    static createWithError: (field: TField, error: ValidationJSONError | string) => ValidationErrors<TField>;
+    static createFromJSON<TField extends string>(
+      jsonObject: Record<
+        TField,
+        readonly (Readonly<ValidationJSONError> | string)[] | (ValidationJSONError | string)[]
+      >
+    ): ValidationErrors<TField>;
+    static createWithError<TField extends string>(
+      field: TField,
+      error: Readonly<ValidationJSONError> | ValidationJSONError | string
+    ): ValidationErrors<TField>;
 
-    add: (field: TField, error: ValidationJSONError | string) => ValidationErrors<TField>;
-    hasError: (field: TField) => boolean;
-    getFieldErrors: (field: TField) => ValidationJSONError[];
-    getFieldErrorMessages: (field: TField) => string[];
-    getFailedFields: () => TField[];
-    isEmpty: () => boolean;
-    clearField: (field: TField) => ValidationErrors<TField>;
-    clear: () => ValidationErrors<TField>;
-    toJSON: () => Record<TField, ValidationJSONError[]>;
-    clone: () => ValidationErrors<TField>;
+    add(
+      field: TField,
+      error: Readonly<ValidationJSONError> | ValidationJSONError | string
+    ): ValidationErrors<TField>;
+    hasError(field: TField): boolean;
+    getFieldErrors(field: TField): Readonly<ValidationJSONError>[];
+    getFieldErrorMessages(field: TField): string[];
+    getFailedFields(): TField[];
+    isEmpty(): boolean;
+    clearField(field: TField): ValidationErrors<TField>;
+    clear(): ValidationErrors<TField>;
+    toJSON(): Record<TField, readonly Readonly<ValidationJSONError>[]>;
+    clone(): ValidationErrors<TField>;
     merge<T extends string>(validationErrors: ValidationErrors<T>): ValidationErrors<TField | T>;
-    getErrors: () => readonly Map<TField, ValidationErrorInstance[]>;
+    getErrors(): ReadonlyMap<TField, readonly Readonly<ValidationJSONError>[]>;
   }
 
-  export class Validator<TRecord> {
-    static create<TRecord>(): Validator<TRecord>;
-    field: (field: keyof TRecord, cb: (value: any) => string | undefined) => Validator<TRecord>;
-    asyncField: (field: keyof TRecord, cb: (value: any) => Promise<string | undefined>) => Validator<TRecord>;
-    fields: (
-      fields: (keyof TRecord)[],
-      cb: (record: Partial<TRecord>, errors: ValidationErrors<keyof TRecord>) => any
-    ) => Validator<TRecord>;
-    asyncFields: (
-      fields: (keyof TRecord)[],
-      cb: (record: Partial<TRecord>, errors: ValidationErrors<keyof TRecord>) => Promise<any>
-    ) => Validator<TRecord>;
-    asyncDependence: (fields: (keyof TRecord)[]) => Validator<TRecord>;
-    getValidationDependency: (fields: (keyof TRecord)[]) => (keyof TRecord)[];
-    isValidRecord: (record: Partial<TRecord>) => Promise<ValidationErrors<keyof TRecord>>;
+  export class Validator<TRecord extends Record<string, unknown>> {
+    static create<TRecord extends Record<string, unknown>>(): Validator<TRecord>;
+    field(field: keyof TRecord & string, cb: (value: any) => string | undefined): Validator<TRecord>;
+    asyncField(
+      field: keyof TRecord & string,
+      cb: (value: any) => Promise<string | undefined>
+    ): Validator<TRecord>;
+    fields(
+      fields: readonly (keyof TRecord & string)[],
+      cb: (record: Partial<TRecord>, errors: ValidationErrors<keyof TRecord & string>) => void
+    ): Validator<TRecord>;
+    asyncFields(
+      fields: readonly (keyof TRecord & string)[],
+      cb: (record: Partial<TRecord>, errors: ValidationErrors<keyof TRecord & string>) => Promise<void>
+    ): Validator<TRecord>;
+    asyncDependence(fields: readonly (keyof TRecord & string)[]): Validator<TRecord>;
+    getValidationDependency(fields: readonly (keyof TRecord & string)[]): (keyof TRecord & string)[];
+    isValidRecord(record: Partial<TRecord>): Promise<ValidationErrors<keyof TRecord & string>>;
+  }
+
+  class AbstractFormModel<TRecord extends Record<string, unknown>> {
+    protected trigger(event: string, ...params: any[]): void;
+
+    getData<TField extends keyof TRecord & string>(
+      fields: readonly TField[] | TField[]
+    ): Promise<Pick<TRecord, TField>>;
+    submit(record: Partial<TRecord>): Promise<Partial<TRecord>>;
+    isValidRecord(record: Partial<TRecord>): Promise<ValidationErrors<keyof TRecord & string>>;
+    getValidationDependency(
+      fields: readonly (keyof TRecord & string)[] | (keyof TRecord & string)[]
+    ): (keyof TRecord & string)[];
+    on(event: string, cb: (...args: any[]) => void): void;
+    off(event: string, cb: (...args: any[]) => void): void;
   }
 
   export interface FormStateEmpty {
@@ -52,7 +79,7 @@ declare module 'uikernel' {
     warnings: ValidationErrors<never>;
   }
 
-  export interface FormState<TRecord, TField extends keyof TRecord> {
+  export interface FormState<TRecord, TField extends keyof TRecord & string> {
     isLoaded: boolean;
     isSubmitting: boolean;
     data: Partial<TRecord>;
@@ -66,53 +93,60 @@ declare module 'uikernel' {
         warnings: ReturnType<ValidationErrors<TField>['getFieldErrors']>;
       };
     };
-    errors: ValidationErrors<keyof TRecord>;
-    warnings: ValidationErrors<keyof TRecord>;
+    errors: ValidationErrors<keyof TRecord & string>;
+    warnings: ValidationErrors<keyof TRecord & string>;
   }
 
-  export interface FormSettings<TRecord, TField extends keyof TRecord> {
+  export interface FormSettings<
+    TRecord extends Record<string, unknown>,
+    TField extends keyof TRecord & string
+  > {
     fields: readonly TField[];
-    model: any;
-    data?: readonly Partial<TRecord>;
-    changes?: readonly Partial<TRecord>;
+    model: typeof AbstractFormModel;
+    data?: Readonly<Partial<TRecord>>;
+    changes?: Readonly<Partial<TRecord>>;
     submitAll?: boolean;
     partialErrorChecking?: boolean;
     showDependentFields?: boolean;
     warningsValidator?: Validator<TRecord>;
   }
 
-  export class FormService<TRecord, TField extends keyof TRecord> {
-    init: (settings: FormSettings<TRecord, TField>) => Promise<void>;
-    updateField: (field: TField, value: any) => Promise<void>;
-    validateField: (field: TField, value: any) => Promise<void>;
-    validateForm: () => Promise<{
-      errors: ValidationErrors<keyof TRecord> | null;
-      warnings: ValidationErrors<keyof TRecord> | null;
+  export class FormService<TRecord extends Record<string, unknown>, TField extends keyof TRecord & string> {
+    init(settings: FormSettings<TRecord, TField>): Promise<void>;
+    updateField(field: TField, value: any): Promise<void>;
+    validateField(field: TField, value: any): Promise<void>;
+    validateForm(): Promise<{
+      errors: ValidationErrors<keyof TRecord & string> | null;
+      warnings: ValidationErrors<keyof TRecord & string> | null;
     }>;
-    submit: () => Promise<Partial<TRecord>>;
-    submitData: (data: Partial<TRecord>) => Promise<Partial<TRecord>>;
-    set: (data: Partial<TRecord>, validate: boolean) => Promise<void>;
+    submit(): Promise<Partial<TRecord>>;
+    submitData(data: Partial<TRecord>): Promise<Partial<TRecord>>;
+    set(data: Partial<TRecord>, validate: boolean): Promise<void>;
 
-    getAll: () => FormStateEmpty | FormState<TRecord, TField>;
-    setPartialErrorChecking: (value: boolean) => void;
-    getPartialErrorChecking: () => boolean;
-    clearValidation: (fields: TField | TField[]) => void;
-    clearFieldChanges: (field: TField) => void;
-    clearChanges: () => void;
-    addChangeListener: (func: (state: ReturnType<FormService<TRecord, TField>['getAll']>) => void) => void;
-    removeChangeListener: (func: (state: ReturnType<FormService<TRecord, TField>['getAll']>) => void) => void;
-    removeAllListeners: () => void;
+    getAll(): FormStateEmpty | FormState<TRecord, TField>;
+    setPartialErrorChecking(value: boolean): void;
+    getPartialErrorChecking(): boolean;
+    clearValidation(fields: readonly TField[] | TField[] | TField): void;
+    clearFieldChanges(field: TField): void;
+    clearChanges(): void;
+    addChangeListener(func: (state: ReturnType<FormService<TRecord, TField>['getAll']>) => void): void;
+    removeChangeListener(func: (state: ReturnType<FormService<TRecord, TField>['getAll']>) => void): void;
+    removeAllListeners(): void;
   }
 
   interface UIKernel {
     Validator: typeof Validator;
-    createValidator: <TRecord>() => Validator<TRecord>;
+    createValidator: <TRecord extends Record<string, unknown>>() => Validator<TRecord>;
     ValidationErrors: typeof ValidationErrors;
     Form: typeof FormService;
-    useForm: <TRecord, TField extends keyof TRecord>(
+    useForm: <TRecord extends Record<string, unknown>, TField extends keyof TRecord & string>(
       setting: FormSettings<TRecord, TField>,
       onError: (error: Error) => void
     ) => [ReturnType<FormService<TRecord, TField>['getAll']>, FormService<TRecord, TField>];
+    AbstractModels: {
+      Form: typeof AbstractFormModel;
+      [key: string]: any;
+    };
     [key: string]: any;
   }
 
