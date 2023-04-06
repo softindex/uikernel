534f76635278fdaf1e89bda8c205b9511a8d976f
dmitry <dmitri.blyus@gmail.com>
2023-04-06 16:13:02 +0300

    Fixed build_examples.mjs: Init directories in _site/examples

--- scripts/build_examples.mjs
+++ scripts/build_examples.mjs
@@ -6,14 +6,18 @@ const dirNames = fs
   .readdirSync('./examples', { withFileTypes: true })
   .filter(dir => dir.isDirectory() && dir.name !== 'libs')
   .map(dir => dir.name);
+
+// Fix of yarn error. Need to clear directories before `yarn install`
 for (const dirName of dirNames) {
   await $`cd examples/${dirName} && rm -rf node_modules build .yarn`;
 }
+
 for (const dirName of dirNames) {
   await $`rm -rf _site/examples/${dirName}/* \
     && cd examples/${dirName} \
     && yarn install \
     && yarn run build \
+    && mkdir -p ../../_site/examples/${dirName}\
     && mv -v build/* ../../_site/examples/${dirName} \
     && rm -rf node_modules .yarn build`;
 }
