402d87006501f4162a6e36a08939c957f9aa6796
nelia-kunyk-soft-index <nkunyk@adkernel.com>
2023-04-06 15:04:43 +0300

    fixed displaying examples

--- scripts/build_examples.mjs
+++ scripts/build_examples.mjs
@@ -6,14 +6,16 @@ const dirNames = fs
   .readdirSync('./examples', { withFileTypes: true })
   .filter(dir => dir.isDirectory() && dir.name !== 'libs')
   .map(dir => dir.name);
-
 for (const dirName of dirNames) {
-  await $`rm -rf _site/examples/${dirName} \
-      && cd examples/${dirName} \
-      && npm install \
-      && npm run build \
-      && mkdir -p ../../_site/examples/${dirName}\
-      && mv -v build/* ../../_site/examples/${dirName}`;
+  await $`cd examples/${dirName} && rm -rf node_modules build .yarn`;
+}
+for (const dirName of dirNames) {
+  await $`rm -rf _site/examples/${dirName}/* \
+    && cd examples/${dirName} \
+    && yarn install \
+    && yarn run build \
+    && mv -v build/* ../../_site/examples/${dirName} \
+    && rm -rf node_modules .yarn build`;
 }
 
 await $`cd examples/final-example && cp src/main.css ../../_site/static/css/final-example.css`;
