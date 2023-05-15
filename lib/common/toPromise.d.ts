58e793fa606286151f3e9194403435b668ecc6ac
Stanislav <stasizon@gmail.com>
2023-05-15 11:22:50 +0300

    v1.0.1-dev13.7

---
+++ lib/common/toPromise.d.ts
@@ -0,0 +1,2 @@
+export default toPromise;
+declare function toPromise(func: any, hideWarning: any): (...mainArguments: any[]) => Promise<any>;
---
+++ lib/form/adapters/GridToFormUpdate.d.ts
@@ -0,0 +1,20 @@
+import { EventListener, IObservable } from '../../common/types';
+import { GridModelListenerArgsByEventName } from '../../grid/models/types/GridModelListenerArgsByEventName';
+import { IGridModel } from '../../grid/models/types/IGridModel';
+import ValidationErrors from '../../validation/ValidationErrors';
+import { FormModelListenerArgsByEventName } from '../types/FormModelListenerArgsByEventName';
+import { IFormModel } from '../types/IFormModel';
+declare class GridToFormUpdate<TKey, TRecord extends Record<string, unknown>, TFilters> implements IFormModel<TRecord>, IObservable<FormModelListenerArgsByEventName<TRecord>> {
+    private gridModel;
+    private id;
+    private onUpdateHandlers;
+    constructor(gridModel: IGridModel<TKey, TRecord, TFilters> & IObservable<GridModelListenerArgsByEventName<TKey, TRecord>>, id: TKey);
+    getData(): Promise<Partial<TRecord>>;
+    getData<TField extends keyof TRecord & string>(fields: TField[]): Promise<Partial<Pick<TRecord, TField>>>;
+    submit(changes: Partial<TRecord>): Promise<Partial<TRecord>>;
+    isValidRecord(record: Partial<TRecord>): Promise<ValidationErrors<keyof TRecord & string>>;
+    getValidationDependency(fields: (keyof TRecord & string)[]): (keyof TRecord & string)[];
+    on<TEventName extends keyof FormModelListenerArgsByEventName<TRecord>>(eventName: TEventName, cb: EventListener<FormModelListenerArgsByEventName<TRecord>[TEventName]>): this;
+    off<TEventName extends keyof FormModelListenerArgsByEventName<TRecord>>(eventName: TEventName, cb: EventListener<FormModelListenerArgsByEventName<TRecord>[TEventName]>): this;
+}
+export default GridToFormUpdate;
