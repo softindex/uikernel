9085cf8abc533342b962deffb47311e2aeccdc02
dmitry <dmitri.blyus@gmail.com>
2023-06-08 16:52:49 +0300

    v1.0.1-dev13.8

---
+++ lib/grid/export/exportGridData.d.ts
@@ -0,0 +1,24 @@
+import { AnyFunction } from '../../common/types';
+import { IGridModel, GridModelSortMode } from '../models/types/IGridModel';
+import { GridColumns } from '../types/GridColumns';
+type FormatColumnsResult<TField extends string> = Record<TField, string>;
+type FormatRecordResult<TField extends string> = Record<TField, string>;
+type FormatDataResult<TColumn extends string> = {
+    columns: FormatColumnsResult<TColumn>;
+    records: FormatRecordResult<TColumn>[];
+    totals?: FormatRecordResult<TColumn>;
+};
+export type ExportGridDataParams<TRecord extends Record<string, unknown>> = {
+    limit?: number;
+    offset?: number;
+    sort?: {
+        column: keyof TRecord & string;
+        direction: GridModelSortMode;
+    };
+};
+export type ExportGridDataResult<TExportRunner extends AnyFunction> = TExportRunner extends AnyFunction<Promise<infer RESULT>> ? RESULT : ReturnType<TExportRunner>;
+declare function exportGridData<TRecord extends Record<string, unknown>, TColumn extends string, TViewColumn extends TColumn, TExportRunner extends (data: FormatDataResult<TViewColumn>) => Promise<{
+    data: unknown;
+    mime: string;
+}>>(gridModel: IGridModel<unknown, TRecord, unknown>, columns: Partial<GridColumns<unknown, TRecord, TColumn>>, viewColumns: TViewColumn[], exportRunner: TExportRunner, settings: ExportGridDataParams<TRecord>): Promise<ExportGridDataResult<TExportRunner>>;
+export default exportGridData;
---
+++ lib/grid/models/AbstractGridModel.d.ts
@@ -0,0 +1,16 @@
+import { EventListener, IObservable } from '../../common/types';
+import ValidationErrors from '../../validation/ValidationErrors';
+import { GridModelReadParams, IGridModel, GridModelReadResult, GridModelUpdateResult } from './types/IGridModel';
+declare abstract class AbstractGridModel<TKey, TRecord extends Record<string, unknown>, TFilters, TListenerArgsByEventName extends Record<string, unknown[]>> implements IGridModel<TKey, TRecord, TFilters>, IObservable<TListenerArgsByEventName> {
+    private eventsModel;
+    create(_record: Partial<TRecord>): Promise<TKey>;
+    read<TField extends keyof TRecord & string>(_params: GridModelReadParams<TKey, TRecord, TField, TFilters>): Promise<GridModelReadResult<TKey, TRecord, TField>>;
+    getRecord<TField extends keyof TRecord & string>(_id: TKey, _fields: TField[]): Promise<Pick<TRecord, TField> | null>;
+    update(_changes: [TKey, Partial<TRecord>][]): Promise<GridModelUpdateResult<TKey, TRecord>>;
+    isValidRecord(_record: Partial<TRecord>, _recordId?: TKey | null): Promise<ValidationErrors<keyof TRecord & string>>;
+    getValidationDependency(_fields: (keyof TRecord & string)[]): (keyof TRecord & string)[];
+    on<TEventName extends keyof TListenerArgsByEventName & string>(eventName: TEventName, cb: EventListener<TListenerArgsByEventName[TEventName]>): this;
+    off<TEventName extends keyof TListenerArgsByEventName & string>(eventName: TEventName, cb: EventListener<TListenerArgsByEventName[TEventName]>): this;
+    protected trigger<TEventName extends keyof TListenerArgsByEventName & string>(eventName: TEventName, ...args: TListenerArgsByEventName[TEventName]): void;
+}
+export default AbstractGridModel;
